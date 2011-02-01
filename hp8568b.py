e22806536ca04fedc399a6e260c2c7ff21b645d0
Poul-Henning Kamp <phk@FreeBSD.org>
2011-02-01 20:47:42 +0000

    Add a screen_dump method for HP8568B

--- hp8568b.py
+++ hp8568b.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import prologix_usb
+import subprocess
 
 class hp8568b(prologix_usb.gpib_dev):
 
@@ -13,9 +14,26 @@ class hp8568b(prologix_usb.gpib_dev):
 			self.fail("HP8568B ID failure (" + x + ")")
 		self.id = x
 
+	def screen_dump(self, fname="_.hp8568b.eps", format="eps"):
+		print(self.id + " Taking a " + format +
+		    " screendump into " + fname)
+		x=self.ask("PLOT 0,0,40000,40000")
+		p = subprocess.Popen([
+			"hp2xx",
+			"-m", format,
+			"-f", fname,
+			"-w180",
+			"-o5",
+			"-O5",
+			"-c42145670",
+			"-p11111111",
+			"-"
+			], stdin=subprocess.PIPE)
+		p.stdin.write(x)
+		p.stdin.close()
+		p.wait()
+
 
 if __name__ == "__main__":
 	d=hp8568b()
 	print("Device responds: " + d.ask("ID") + " Rev: " + d.ask("REV"))
-
-
